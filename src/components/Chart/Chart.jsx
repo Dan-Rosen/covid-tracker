7a5fd2578fb1507324363b3649ea87262f881b97
Dan Rosen <drosen@precipart.com>
2020-05-10 17:02:51 -0400

    bar chart and country picker

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -5,18 +5,17 @@ import { fetchDailyData } from '../../api'
 
 import styles from './Chart.module.css'
 
-const Chart = () => {
+const Chart = ({ data: { confirmed, deaths, recovered }, country }) => {
     const [dailyData, setDailyData] = useState([])
 
     useEffect(() => {
         const fetchAPI = async () => {
-            const initialDailyData = await fetchDailyData()
-            setDailyData(initialDailyData)
+            setDailyData(await fetchDailyData())
         }
         fetchAPI()
     }, [])
 
-    const lineChart = dailyData[0] ? (
+    const lineChart = dailyData.length ? (
         <Line
             data={{
                 labels: dailyData.map(({ date }) => date),
@@ -39,7 +38,32 @@ const Chart = () => {
         />
     ) : null
 
-    return <div className={styles.container}>{lineChart}</div>
+    const barChart = confirmed ? (
+        <Bar
+            data={{
+                labels: ['Infected', 'Recovered', 'Deaths'],
+                datasets: [
+                    {
+                        label: 'People',
+                        backgroundColor: [
+                            'rgba(0, 0, 255, 0.5)',
+                            'rgba(0, 255, 0, 0.5)',
+                            'rgba(255, 0, 0, 0.5)'
+                        ],
+                        data: [confirmed.value, recovered.value, deaths.value]
+                    }
+                ]
+            }}
+            options={{
+                legend: { display: false },
+                title: { display: true, text: `Current state in ${country}` }
+            }}
+        />
+    ) : null
+
+    return (
+        <div className={styles.container}>{country ? barChart : lineChart}</div>
+    )
 }
 
 export default Chart
