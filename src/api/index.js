f83eaf8f3841c78ab7cdca326cc626a2576a563c
Dan Rosen <drosen@precipart.com>
2020-05-10 16:15:47 -0400

    chart data and styles

--- src/api/index.js
+++ src/api/index.js
@@ -14,5 +14,21 @@ export const fetchData = async () => {
             deaths,
             lastUpdate
         }
-    } catch (error) {}
+    } catch (error) {
+        return error
+    }
+}
+
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`)
+
+        return data.map(({ confirmed, deaths, reportDate: date }) => ({
+            confirmed: confirmed.total,
+            deaths: deaths.total,
+            date
+        }))
+    } catch (error) {
+        return error
+    }
 }
